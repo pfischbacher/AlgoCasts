4e143688cd7409c46334cd96ac86992ac05862d8
Philip Fischbacher <pfischbacher@gmail.com>
2020-02-28 12:23:40 +0900

    completed reversestring exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+function reverse(str) {
+  return str.split('').reduce(
+    (rev, char) => {
+      return char + rev;
+    }, '' );
+}
 
 module.exports = reverse;
+
+//function reverse(str) {
+//  let reversed = '';
+//  let arr = str.split('');
+//  reversed = arr.reverse();
+//  return reversed.join('');
+//}
+
+
+
+//function reverse(str) {
+//  let reversed = '';
+//  for (let i = str.length - 1; i >=0; i--) {
+//    reversed += str[i];  
+//  }
+//  return reversed;
+//}
