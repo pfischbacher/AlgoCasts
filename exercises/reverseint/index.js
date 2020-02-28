a111e1e466832fe4ec594318fd41a11d8b48b271
Philip Fischbacher <pfischbacher@gmail.com>
2020-02-28 18:02:47 +0900

    completed reverseint exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let result = 0;
+  let sign = 1;
+
+  if (n < 0) {
+    n = 0 - n;
+    sign = -1;
+  }
+
+  while ( n > 0) {
+    result *= 10;
+    result += n % 10;
+    n = (n - (n % 10) ) / 10;
+  }
+
+  result *= sign;
+
+  return result;
+}
 
 module.exports = reverseInt;
+
+//function reverseInt(n) {
+//  return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''));
+//}
