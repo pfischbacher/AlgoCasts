91a9648ac3201146cde93d0a5a61af7f07f2ef6c
Philip Fischbacher <pfischbacher@gmail.com>
2020-02-28 18:02:08 +0900

    added palindrome exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+function palindrome(str) {
+  return str.split('').every((char, i) => char === str[str.length - i -1]);
+}
 
 module.exports = palindrome;
+
+//function palindrome(str) {
+//  return str === str.split('').reverse().join('');
+//}
+
+//function palindrome(str) {
+//  let arr = str.split('');
+//  let len = arr.length;
+//  let result = true;
+//  for (let i = 0; i < Math.floor(len / 2) + 1; i++) {
+//    if (arr[i] != arr[len - i - 1] ) {
+//      return false;
+//    }
+//  }
+//
+//  return result;
+//}
