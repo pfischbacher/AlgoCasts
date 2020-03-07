719938a8f653fe588842bd1d60157426aba91535
Philip Fischbacher <pfischbacher@gmail.com>
2020-03-07 21:54:48 +0900

    completed vowels exercise

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,34 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const result = str.match(/[aeiou]/gi);
+
+  return result ? result.length : 0;
+}
 
 module.exports = vowels;
+
+//function vowels(str) {
+//  const vowelsString = 'aeiou';
+//  let result = 0;
+//  for (let char of str.toLowerCase()) {
+//    for (let vowel of vowelsString) {
+//      if (vowel === char) {
+//        result++;
+//      }
+//    }
+//  }
+//  return result;
+//}
+
+//function vowels(str) {
+//  const checker = ['a','e','i','o','u'];
+//  let result = 0;
+//  for (let char of str.toLowerCase()) {
+//    if (checker.includes(char)) {
+//      result++;
+//    }
+//  }
+//  return result;
+//}
