b311aacfae14c8c9044940238a4d9eff614a2ec6
Philip Fischbacher <pfischbacher@gmail.com>
2020-03-07 17:32:47 +0900

    completed steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,27 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, i = 1) {
+  console.log('#'.repeat(i) + ' '.repeat(n - i));
+  if ( i < n ) {
+    steps(n, i+1);
+  }
+}
 
 module.exports = steps;
+
+//function steps(n) {
+//  for (let i = 0; i < n; i++) {
+//    console.log('#'.repeat( i + 1) + ' '.repeat(n - i - 1));
+//  }
+//}
+
+//function steps(n) {
+//  for (let row = 0; row < n; row++) {
+//    let cols = [];
+//    for (let col = 0; col < n; col++) {
+//      cols[col] = col <= row ? '#' : ' ';
+//    }
+//    console.log(cols.join(''));
+//  }
+//}
