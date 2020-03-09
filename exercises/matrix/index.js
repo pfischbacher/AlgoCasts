c5db478990e67fa4e8559959ce2e84a54deed7d7
Philip Fischbacher <pfischbacher@gmail.com>
2020-03-09 17:17:15 +0900

    completed matrix exercise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -10,11 +10,44 @@
 //     [8, 9, 4],
 //     [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4],
+//     [[1,  2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(Array(n));
+  }
+  const dir = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+  let dirIndex = 0;
+  let col = 0;
+  let row = 0;
+  let count = 0;
+  let turnCounter = n;
+  let turns = 1;
+
+  for (let i = 1; i <= n * n; i++) {
+    result[row][col] = i;
+    if (count === turnCounter - 1 ) {
+      count = 0;
+      dirIndex = dirIndex < 3 ? dirIndex + 1 : 0;
+      // reduce turnCounter every 2nd turn
+      if (turns === 1) {
+        turnCounter--;
+        turns = 0;
+      } else {
+        turns++;
+      }
+    } else {
+      count++;
+    }
+
+    row += dir[dirIndex][0];
+    col += dir[dirIndex][1];
+  }
+  return result;
+}
 
 module.exports = matrix;
