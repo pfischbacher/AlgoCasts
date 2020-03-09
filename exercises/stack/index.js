c34af1be685d61f434bbadd1d9600cea6b8cefe6
Philip Fischbacher <pfischbacher@gmail.com>
2020-03-09 22:28:32 +0900

    completed stack exercise

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this._data = [];
+  }
+
+  push(record) {
+    this._data.push(record);
+  }
+
+  pop() {
+    return this._data.pop();
+  }
+
+  peek() {
+    return this._data[this._data.length - 1];
+  }
+}
 
 module.exports = Stack;
