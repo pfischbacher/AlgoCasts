9aaf805a0927eaba8e5115a544d6bd432544c3d1
Philip Fischbacher <pfischbacher@gmail.com>
2020-03-13 22:40:16 +0900

    completed circular exercise

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,20 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst().next;
+
+  while (fast.next && fast.next.next && slow !== fast) {
+    slow = slow.next
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  
+  return false;
+
+}
 
 module.exports = circular;
