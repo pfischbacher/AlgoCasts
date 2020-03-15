7b1a7e6cf992d5b2ca02a05c05ddb6afa54fe6f1
Philip Fischbacher <pfischbacher@gmail.com>
2020-03-15 22:36:26 +0900

    completed validate exercise

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,32 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (min) {
+    if (min > node.data) {
+      return false;
+    }
+  }
+
+  if (max) {
+    if (max < node.data) {
+      return false;
+    }
+  }
+
+  if (node.left) {
+    if (!validate(node.left, min, node.data)) {
+      return false;
+    }
+  }
+
+  if (node.right) {
+    if(!validate(node.right, node.data, max)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = validate;
